9c5467e8119a00e3f2bb263440afeee1a2005d74
Jose Tellez <48417377+Rhaegar2012@users.noreply.github.com>
2024-09-11 06:44:34 -0700

    20240911-Update
    Added persistent delete functionality

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9f41aeae08eaadad63f7c762888f56bc09b86ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73ceefdef5d93ea17dab8a59e995aa45a6b2dfc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
