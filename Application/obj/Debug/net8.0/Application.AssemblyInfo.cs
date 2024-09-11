73ceefdef5d93ea17dab8a59e995aa45a6b2dfc5
Jose Tellez <48417377+Rhaegar2012@users.noreply.github.com>
2024-09-11 05:58:55 -0700

    20240911-Update
    Added functionality to write and modify entries and persist them to the database

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+619ed37fdcd71bfea6d462161e35482cb446f90c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9f41aeae08eaadad63f7c762888f56bc09b86ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -19,9 +19,10 @@ interface Props
   closeForm:()=>void;
   createOrEdit:(activity:Activity)=>void;
   deleteActivity:(id:string)=>void;
+  submitting:boolean;
 
 }
-export default function ActivityDashboard({activities,selectedActivity,selectActivity,cancelSelectActivity,editMode,openForm,closeForm,createOrEdit,deleteActivity}:Props){
+export default function ActivityDashboard({activities,selectedActivity,selectActivity,cancelSelectActivity,editMode,openForm,closeForm,createOrEdit,deleteActivity,submitting}:Props){
     return(
             <Grid>
                 <Grid.Column width = '10'>
@@ -37,7 +38,7 @@ export default function ActivityDashboard({activities,selectedActivity,selectAct
                         cancelSelectActivity={cancelSelectActivity}
                         openForm={openForm}/>}
                     {editMode &&
-                    <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit}/>}
+                    <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit} submitting={submitting}/>}
                 </Grid.Column>
             </Grid>
     )
