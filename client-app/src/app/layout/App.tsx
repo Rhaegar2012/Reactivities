5a6a93e46824a903a0f754632f9f4156800cd636
Jose Tellez <48417377+Rhaegar2012@users.noreply.github.com>
2024-10-15 06:54:20 -0700

    20241015-Update

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -5,12 +5,26 @@ import {Container} from "semantic-ui-react"
 import {observer} from 'mobx-react-lite'
 import { Outlet,useLocation } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
+import { useStore } from '../stores/store'
+import { useEffect } from 'react'
+import LoadingComponent from './LoadingComponents'
 
 
 
 function App() {
 
   const location = useLocation();
+  const {commonStore,userStore} = useStore();
+
+  useEffect(()=>{
+    if(commonStore.token){
+      userStore.getUser().finally(()=>commonStore.setAppLoaded())
+    }else{
+      commonStore.setAppLoaded()
+    }
+  },[commonStore,useStore])
+
+  if(!commonStore.appLoaded) return <LoadingComponent content ='Loading app...'/>
 
   return (
     
--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -1,11 +1,13 @@
-import {Container, Menu,Button} from "semantic-ui-react"; 
-import {NavLink} from 'react-router-dom';
+import {Container, Menu,Button,Image, Dropdown} from "semantic-ui-react"; 
+import {Link, NavLink} from 'react-router-dom';
+import { useStore } from "../stores/store";
+import { observer } from "mobx-react-lite";
 
 
 
-export default function NavBar()
+export default observer(function NavBar()
 {
-    
+    const {userStore:{user, logout}} =useStore()
     return (  <Menu inverted fixed='top'>
         <Container>
             <Menu.Item as={NavLink} to='/' header>
@@ -17,7 +19,17 @@ export default function NavBar()
             <Menu.Item>
                 <Button as={NavLink} to='/createActivity' positive content="Create Activity"></Button>
             </Menu.Item>
+            <Menu.Item position='right'>
+                <Image src ={user?.image || '/assets/user.png'} avatar spaced ='right'/>
+                <Dropdown pointing ='top left' text ={user?.displayname}>
+                    <Dropdown.Menu>
+                        <Dropdown.Item as={Link} to={`/profile/${user?.username}`} text='My Profile' icon='user'/>
+                        <Dropdown.Item onClick={logout} text='Logout' icon='power'/>
+                    </Dropdown.Menu>
+                </Dropdown>
+            </Menu.Item>
         </Container>
-    </Menu>)
+    </Menu>
+    )
       
-}
+})
