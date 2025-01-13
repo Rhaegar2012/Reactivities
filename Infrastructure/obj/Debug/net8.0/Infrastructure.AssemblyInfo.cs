1203e29b753fb6ae960b2d4b88dfa12dfbc167d4
Jose Tellez <48417377+Rhaegar2012@users.noreply.github.com>
2025-01-13 06:50:18 -0800

    20250113-Update

--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+575ac8ffb11e1ac24eae5cf78584fc0661c702f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8531e11aab18cabc15943a56956dc618c57c3d44")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
